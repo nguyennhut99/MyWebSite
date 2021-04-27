f09885b87261383e43d10cd20829634e32b5ff02
nguyennhut99 <nguyennhut895@gmail.com>
2021-04-27 13:31:15 +0700

    add BrandControllerTest.cs

---
+++ test/UnitTest/BrandControllerTest.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using Xunit;
+using MyShop.Backend.Controllers;
+using MyShop.Share;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using MyShop.Backend.Models;
+
+namespace MyShop.Backend.Tests
+{
+    public class BrandControllerTests : IClassFixture<SqliteInMemoryFixture>
+    {
+        private readonly SqliteInMemoryFixture _fixture;
+
+        public BrandControllerTests(SqliteInMemoryFixture fixture)
+        {
+            _fixture = fixture;
+            _fixture.CreateDatabase();
+        }
+
+        [Fact]
+        public async Task PostBrand_Success()
+        {
+            var dbContext = _fixture.Context;
+            var brand = new BrandCreateRequest {Name = "Test brand"};
+
+            var controller = new BrandController(dbContext);
+            var result = await controller.PostBrand(brand);
+
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+            var returnValue = Assert.IsType<BrandVm>(createdAtActionResult.Value);
+            Assert.Equal("Test brand", returnValue.Name);
+        }
+
+        [Fact]
+        public async Task GetBrand_Success()
+        {
+            var dbContext = _fixture.Context;
+            dbContext.Brands.Add(new Brand { Name = "Test brand" });
+            await dbContext.SaveChangesAsync();
+
+            var controller = new BrandController(dbContext);
+            var result = await controller.GetBrands();
+
+            var actionResult = Assert.IsType<ActionResult<IEnumerable<BrandVm>>>(result);
+            Assert.NotEmpty(actionResult.Value);
+        }
+    }
+}
